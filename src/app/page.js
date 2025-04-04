4c243b111de4be04978f0e59c718dc624d0e5b39
unknown <edwinrmrjr@gmail.com>
2025-04-05 03:04:22 +0800

    Your First API request from Next to Django

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,21 @@
+"use client";
 import Image from "next/image";
 
 export default function Home() {
+  async function getDjangoAPIData() {
+    const response = await fetch("http://127.0.0.1:8000/api/hello");
+    const data = await response.json();
+    console.log(data);
+  }
+
+  async function handleClick() {
+    await getDjangoAPIData();
+  }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <button onClick={handleClick}>
+        click me
+      </button>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
