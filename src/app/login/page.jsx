ae25ddf2d3c9abc8c2c240125a251dadaa5ab36a
unknown <edwinrmrjr@gmail.com>
2025-04-30 12:00:00 +0800

    Login required redirect for invalid auth tokens

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,12 +1,10 @@
 "use client";
 import { useAuth } from "@/components/authProvider";
 // -> url -> /login
-import { useRouter } from "next/navigation";
 const LOGIN_URL = "/api/login/";
 
 export default function Page() {
   const auth = useAuth();
-  const router = useRouter();
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -26,7 +24,6 @@ export default function Page() {
     if (response.ok) {
       console.log("logged in");
       auth.login()
-      router.replace("/");
     }
   }
 
