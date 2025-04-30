e2fd76a27b9cb08ae3a6f4e4c72fe3a7e10d147a
unknown <edwinrmrjr@gmail.com>
2025-04-30 09:07:37 +0800

    Custom Context Provider and useAuth Hook

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 "use client";
+import { useAuth } from "@/components/authProvider";
 import Image from "next/image";
 import { useState } from "react";
 import useSWR from 'swr'
@@ -7,23 +8,15 @@ import useSWR from 'swr'
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 export default function Home() {
-  // GET REQUEST SWR
+  const auth = useAuth();
   const {data, error, isLoading} = useSWR("http://127.0.0.1:8000/api/hello", fetcher)
   if (error) return <div>failed to load</div>
   if (isLoading) return <div>loading...</div>
-  // const [data, setData] = useState({});
-  // async function getDjangoAPIData() {
-  //   const response = await fetch("http://127.0.0.1:8000/api/hello");
-  //   const responseData = await response.json();
-  //   // console.log(data);
-  //   setData(responseData);
-  // }
-
-  // async function handleClick() {
-  //   await getDjangoAPIData();
-  // }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div>
+        {auth.isAuthenticated ? "Hello user" : "Hello guest"}
+      </div>
       <div>
         {JSON.stringify(data)}
       </div>
